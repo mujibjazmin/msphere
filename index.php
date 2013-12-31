dd5d6aef2080b98c6229966686214b9564e80643
Mujib Jazmin <mujibjazmin@gmail.com>
2013-12-31 13:43:07 +0800

    Final echo

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php>
 
 echo "Hello World";
-echo "Selamat Datang"
-<<<<<<< HEAD
-echo "Bye Bye"
-=======
->>>>>>> 87a3258cfc8e41a0f9582035ad7d0ccc3a86a99b
+echo "Selamat Datang";
+echo "Bye Bye";
 
 ?>
